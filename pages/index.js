8fcdab5d91f17588c1c312131fed4e0136aa28fe
Conor C. Peterson <conorpetersondev@gmail.com>
2023-04-03 20:11:10 +0200

    fix: change chart container height
    Express chart container height in svh instead of vh to account for dynamic search bars on mobile device browsers.

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,11 @@ export default function Home() {
     <>
       <Head>
         <title>Pokemon Type Chart</title>
-        <meta
-          name="description"
-          content="Interactive Pokemon type chart created with Next.js and Tailwind"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Responsive Pokemon type chart' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
+      <main className='h-[100svh]'>
         <Chart />
       </main>
     </>
